dd4b0ac986fe504c7c5161b03e3d8ceed0393473
johndoeprojects22 <103950718+johndoeprojects22@users.noreply.github.com>
2022-05-23 11:06:37 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -2,8 +2,11 @@
 import json
 
 def lambda_handler(event, context):
-  exec(open("INSERT").read())
+  #exec(open("INSERT").read())
+  
+  res = subprocess.check_output("INSERT", shell=True).strip()
+  news = str(res,"utf-8")
   return {
         "statusCode": 200,
-        'body': json.dumps('Lambda Deployed')        
+        'body': json.dumps(news)        
   }
