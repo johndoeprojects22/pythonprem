dc7cd95f8305eded9c70be66c6f8db0b36460298
johndoeprojects22 <103950718+johndoeprojects22@users.noreply.github.com>
2022-05-25 15:27:00 -0400

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -1,9 +1,13 @@
 import os
 import random
 import string
-#import boto3 
+import boto3 
 import sys 
 # initializing size of string 
+
+
+
+
 N = 8
   
 # using random.choices()
@@ -12,9 +16,20 @@ res = ''.join(random.choices(string.ascii_lowercase +
                              string.digits, k = N))
 
 
+
+client = boto3.client('lambda')
+
+response = client.list_functions(
+    MasterRegion='us-east-1',
+    FunctionVersion='ALL',
+    MaxItems=10
+)
+
+print(response)
+
+
 command = "aws s3api create-bucket --bucket " + res + " --region us-east-1"
 os.system(command)
-
 
 # Read in the file
 with open('serverless.yaml', 'r') as file:
