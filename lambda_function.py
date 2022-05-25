1732ddedece9b7aaa9f78b1fac654fb00be8b490
johndoeprojects22 <103950718+johndoeprojects22@users.noreply.github.com>
2022-05-25 13:30:22 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,11 +3,18 @@ import json
 import subprocess
 
 def lambda_handler(event, context):
-  #exec(open("INSERT").read())
-  command = "python3 " + "INSERT"
+  #exec(open("hello.py").read())
+  command = "python3 " + "hello.py"
   res = subprocess.check_output(command, shell=True).strip()
   news = str(res,"utf-8")
+  response_body = "<html><body><h1>"+news+"</h1></body></html>"
+    
+    
+    
   return {
-        "statusCode": 200,
-        'body': json.dumps(news)        
+    "statusCode": 200,
+    "body": response_body,
+    "headers": {
+        'Content-Type': 'text/html',
   }
+}
