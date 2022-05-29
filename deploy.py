3face29ab0b74abdf9104928040820007074aefa
johndoeprojects22 <103950718+johndoeprojects22@users.noreply.github.com>
2022-05-28 23:45:54 -0400

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -18,41 +18,41 @@ res = ''.join(random.choices(string.ascii_lowercase +
 stacks = subprocess.check_output('aws cloudformation list-stacks', shell=True) 
 
 content = str(stacks)
-print(contents)
 
+if 'pyprem' in content:
+  print(contents)
 
+else:
 #could be anything here.
+  command = "aws s3api create-bucket --bucket " + res + " --region us-east-1"
+  os.system(command)
+
+  # Read in the file
+  with open('serverless.yaml', 'r') as file:
+    filedata = file.read()
+
+  fname = res
+
+  # Replace the target string
+  filedata = filedata.replace("INSERT", fname)
+
+  # Write the file out again
+  with open('serverless.yaml', 'w') as file:
+    file.write(filedata)
 
 
-command = "aws s3api create-bucket --bucket " + res + " --region us-east-1"
-os.system(command)
-
-# Read in the file
-with open('serverless.yaml', 'r') as file:
-  filedata = file.read()
-
-fname = res
-
-# Replace the target string
-filedata = filedata.replace("INSERT", fname)
-
-# Write the file out again
-with open('serverless.yaml', 'w') as file:
-  file.write(filedata)
-
-  
-os.system("more serverless.yaml")
- 
-  
- 
-destfile = res +"/"
-
-command2 = "aws s3 cp hello.zip s3://"+destfile
-
-os.system(command2)
+  os.system("more serverless.yaml")
 
 
 
-os.system("aws cloudformation deploy --template-file serverless.yaml --stack-name pyprem")
+  destfile = res +"/"
+
+  command2 = "aws s3 cp hello.zip s3://"+destfile
+
+  os.system(command2)
+
+
+
+  os.system("aws cloudformation deploy --template-file serverless.yaml --stack-name pyprem")
 
 
