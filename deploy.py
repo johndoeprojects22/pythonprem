6c325f88ef4f4aa0399f9ffb1660f1b542fbb0f0
johndoeprojects22 <103950718+johndoeprojects22@users.noreply.github.com>
2022-06-01 09:39:18 -0400

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -55,7 +55,7 @@ if 'pyprem' in content:
   
   print("flag 2")
   
-  os.system("aws cloudformation update-stack --template-file serverless.yaml --stack-name pyprem")
+  os.system("aws cloudformation update-stack serverless.yaml pyprem")
 
   
  
