2f4f2c8138276ce5531204a7ca7faadb5e64a816
johndoeprojects22 <103950718+johndoeprojects22@users.noreply.github.com>
2022-06-06 21:45:32 -0400

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,4 +1,15 @@
 import sys
+import boto3
+import os
+
+
+stacks = subprocess.check_output('aws cloudformation list-stacks --stack-status-filter CREATE_COMPLETE', shell=True) 
+
+content = str(stacks)
+
+if 'vpcendpoint' not in content:
+  os.system("aws cloudformation deploy --template-file endpoint.yaml --stack-name vpcendpoint")
+
 
 # Read in the file
 with open('lambda_function.py', 'r') as file:
@@ -11,4 +22,9 @@ filedata = filedata.replace("INSERT", fname)
 
 # Write the file out again
 with open('lambda_function.py', 'w') as file:
+  file.write(filedata)
+
+
+filedata = filedata.replace("INSERT2",)
+with open('serverless.yaml','w') as file:
   file.write(filedata)
